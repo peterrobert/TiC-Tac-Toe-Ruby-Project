66b2f5f93aacf346425aa704e48cf9551a85d654
Sergio <sergio.mauz88@gmail.com>
2020-03-24 11:00:16 -0500

    Fixed return conditional and assignment

--- bin/main.rb
+++ bin/main.rb
@@ -5,13 +5,13 @@
 def print_board(first_player, second_player)
   board = ''
   (0..8).each do |i|
-    if first_player.any?(i + 1)
-      board += ((i + 1) % 3).positive? ? 'X | ' : 'X'
-    elsif second_player.any?(i + 1)
-      board += ((i + 1) % 3).positive? ? 'O | ' : 'O'
-    else
-      board += ((i + 1) % 3).positive? ? '  | ' : ' '
-    end
+    board += if first_player.any?(i + 1)
+               ((i + 1) % 3).positive? ? 'X | ' : 'X'
+             elsif second_player.any?(i + 1)
+               ((i + 1) % 3).positive? ? 'O | ' : 'O'
+             else
+               ((i + 1) % 3).positive? ? '  | ' : ' '
+             end
     board += "\n—————————\n" if ((i + 1) % 3).zero? && i < 8
   end
   puts board
