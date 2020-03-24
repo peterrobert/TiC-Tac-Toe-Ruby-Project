ee5f6f119302edc27c646475eb5bda41c1701762
Sergio <sergio.mauz88@gmail.com>
2020-03-24 03:34:55 -0500

    Solved 10 offenses detected by Rubocop

--- bin/main.rb
+++ bin/main.rb
@@ -1,50 +1,50 @@
 # !/usr/bin/env ruby
-# rubocop: disable Layout/LineLength
+# rubocop: disable Layout/LineLength, Metrics/BlockLength, Metrics/CyclomaticComplexity
+# rubocop: disable Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/BlockNesting
 
 def print_board(first_player, second_player)
-  board = ""
-  for i in 0..8 do
+  board = ''
+  (0..8).each do |i|
     if first_player.any?(i + 1)
-      (i + 1) % 3 > 0 ? board += "X | " : board += "X"
+      board += ((i + 1) % 3).positive? ? 'X | ' : 'X'
     elsif second_player.any?(i + 1)
-      (i + 1) % 3 > 0 ? board += "O | " : board += "O"
+      board += ((i + 1) % 3).positive? ? 'O | ' : 'O'
     else
-      (i + 1) % 3 > 0 ? board += "  | " : board += " "
+      board += ((i + 1) % 3).positive? ? '  | ' : ' '
     end
-    board += "\n—————————\n" if (i + 1) % 3 == 0 && i < 8
+    board += "\n—————————\n" if ((i + 1) % 3).zero? && i < 8
   end
   puts board
 end
 
-def is_valid_movement(first_player_movements, second_player_movements, new_movement)
+def valid_movement?(first_player, second_player, new_movement)
   is_integer = true if Integer(new_movement) rescue false
   if is_integer
-    if new_movement.to_i >= 1 && new_movement.to_i <= 9      
-      return first_player_movements.none?(new_movement) && second_player_movements.none?(new_movement)
+    if new_movement.to_i >= 1 && new_movement.to_i <= 9
+      return first_player.none?(new_movement) && second_player.none?(new_movement)
     end
   end
-  false  
-end 
+  false
+end
 
-def new_movement(player, movement)  
+def new_movement(player, movement)
   player.push(movement.to_i)
 end
 
 def player_win?(player_movements)
-  # ONLY FOR TEST, IT IS NOT THE FINAL METHOD TO DETECT IF A PLAYER WIN  
+  # ONLY FOR TEST, IT IS NOT THE FINAL METHOD TO DETECT IF A PLAYER WON
   return false if player_movements.length < 4
 
   [true, false].sample
 end
 
 def game_finished?(first_player_movements, second_player_movements)
+  return 1 if player_win?(first_player_movements)
+  return 2 if player_win?(second_player_movements)
+
   if first_player_movements.length + second_player_movements.length == 9
-    return 1 if player_win?(first_player_movements)  
-    return 2 if player_win?(second_player_movements)
-    0 
+    0
   else
-    return 1 if player_win?(first_player_movements)  
-    return 2 if player_win?(second_player_movements)
     -1
   end
 end
@@ -75,47 +75,47 @@ if start_game == 'y'
   end
 
   puts "\nHello #{first_player} and #{second_player}, let\'s play..."
-  
-  loop do    
+
+  loop do
     first_player_movements = []
     second_player_movements = []
 
     game_reset = ''
     print_board(first_player_movements, second_player_movements)
-    puts "\n"      
+    puts "\n"
 
     i = 0
     who_won = -1
-    while who_won < 0
-      if i == 0
+    while who_won.negative?
+      if i.zero?
         puts "\nYour turn #{first_player}: "
-        t = gets.chomp
-        
-        while !is_valid_movement(first_player_movements, second_player_movements, t)          
+        movement = gets.chomp
+
+        until valid_movement?(first_player_movements, second_player_movements, movement)
           puts "\nPlease, insert a valid value. Your turn #{first_player}: "
-          t = gets.chomp
-        end                
-        new_movement(first_player_movements, t)
-        
+          movement = gets.chomp
+        end
+        new_movement(first_player_movements, movement)
+
         i += 1
       else
         puts "\nYour turn #{second_player}: "
-        t = gets.chomp
+        movement = gets.chomp
 
-        while !is_valid_movement(first_player_movements, second_player_movements, t)          
+        until valid_movement?(first_player_movements, second_player_movements, movement)
           puts "\nPlease, insert a valid value. Your turn #{second_player}:"
-          t = gets.chomp
-        end           
-        new_movement(second_player_movements, t)
+          movement = gets.chomp
+        end
+        new_movement(second_player_movements, movement)
 
         i -= 1
       end
       print_board(first_player_movements, second_player_movements)
       who_won = game_finished?(first_player_movements, second_player_movements)
     end
-   
+
     if who_won == 1
-      puts "\nGame Over. The winner is #{first_player}."      
+      puts "\nGame Over. The winner is #{first_player}."
     elsif who_won == 2
       puts "\nGame Over. The winner is #{second_player}."
     else
@@ -131,12 +131,5 @@ if start_game == 'y'
   end
 end
 
-=begin
-def is_number?(x)
-  true if Integer(x) rescue false
-end
-
-a = Integer("123")
-p a
-=end
-# rubocop: enable Layout/LineLength
+# rubocop: enable Layout/LineLength, Metrics/BlockLength, Metrics/CyclomaticComplexity
+# rubocop: enable Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/BlockNesting
