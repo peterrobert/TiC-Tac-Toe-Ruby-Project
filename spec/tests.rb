10635e7eeeb9ddaa6892d7f5576a93d309139e5a
Sergio <sergio.mauz88@gmail.com>
2020-03-30 11:32:14 -0500

    Fixed Rubocop offenses in valid_movement? method

--- spec/tests.rb
+++ spec/tests.rb
@@ -1,12 +1,30 @@
+# rubocop: disable Layout/LineLength
+
 require_relative '../lib/game.rb'
 
 RSpec.describe Game do
-    describe "#valid_movement?" do
-        let(:game) {Game.new}
-      it "returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements" do
+  describe '#valid_movement?' do
+    let(:game) { Game.new }
+    it 'returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements' do
+      expect(game.valid_movement?([], [], 9)).to eql(true)
+    end
 
-        expect(game.valid_movement?([],[],9)).to eql(true)
+    it 'returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements' do
+      expect(game.valid_movement?([3], [5], 9)).to eql(true)
+    end
 
-      end
+    it 'returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements' do
+      expect(game.valid_movement?([9], [1, 2], 9)).to eql(false)
+    end
+
+    it 'returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements' do
+      expect(game.valid_movement?([], [], 10)).to eql(false)
+    end
+
+    it 'returns true if it recieves a number between 1 and 9 and this number must be unique in first player and second player movements' do
+      expect(game.valid_movement?([9], [1, 2], 'd')).to eql(false)
     end
   end
+end
+
+# rubocop: enable Layout/LineLength
